138af027fd347fd85f0a0ec6481a6c1cccac9e22
KyouGenSo <k023g0083@g.neec.ac.jp>
2026-07-29 02:39:46 +0900

    change: プレイヤーのHPを180に変更

--- GameProject/Object/Player/Player.h
+++ GameProject/Object/Player/Player.h
@@ -40,7 +40,7 @@ private: //定数
     static constexpr float kMoveEasingCoeffA = 3.0f;
     static constexpr float kMoveEasingCoeffB = 2.0f;
     static constexpr float kDirectionEpsilon = 0.01f;
-    static constexpr float kMaxHp = 150.0f;
+    static constexpr float kMaxHp = 180.0f;
 
 public: //メンバー関数
     Player();
